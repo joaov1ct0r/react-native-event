452778a6299469ac6233303723ccbd2a7f84f374
joaov1ct0r <jov9051@outlook.com>
2023-07-25 21:47:24 -0300

    feat: form

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
       padding: 24
     },
     eventName: {
-      color: '#FDFCFE',
+      color: 'white',
       fontSize: 24,
       fontWeight: 'bold',
       marginTop: 48
@@ -15,5 +15,34 @@ export const styles = StyleSheet.create({
     eventDate: {
       color: '#6B6B6B',
       fontSize: 16
+    },
+    input: {
+      flex: 1,
+      marginRight: 12,
+      height: 56,
+      backgroundColor: '#1F1E25',
+      borderRadius: 5,
+      color: 'white',
+      padding: 16,
+      fontSize: 16
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 24,
+      fontWeight: 'bold'
+    },
+    button: {
+      width: 56,
+      height: 56,
+      borderRadius: 5,
+      backgroundColor: '#31CF67',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    form: {
+      width: '100%',
+      flexDirection: 'row',
+      marginTop: 36,
+      marginBottom: 42
     }
   })
